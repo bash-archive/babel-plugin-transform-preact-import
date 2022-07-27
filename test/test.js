7bf9f9bfa41b8c5ded5279d959d5594815f18062
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-07-27 11:25:08 +0200

    Update babel

--- test/test.js
+++ test/test.js
@@ -1,39 +1,49 @@
 const assert = require('assert')
-const babel = require('babel-core')
+const babel = require('@babel/core')
 const plugin = require('../index').default
+const generate = require('@babel/generator').default
 
 const IMPORT = 'import { h } from "preact";'
 
 const transform = (code) => {
   const result = babel.transform(code, {
-    plugins: [['syntax-jsx', {pragma: 'h'}], plugin],
+    ast: true,
+    plugins: [['@babel/syntax-jsx', {pragma: 'h'}], plugin],
   })
 
-  return result.code
+  return generate(result.ast).code
+}
+
+const normalizeFormatting = (code) => {
+  const ast = babel.parse(code, {
+    plugins: [['@babel/syntax-jsx', { pragma: 'h' }]],
+  })
+
+  return generate(ast).code
 }
 
 describe('babel-plugin-preact-import', function () {
   it('adds an import declaration', function () {
     const code = 'const Title = ({ value }) => <h1>{value}</h1>;'
 
-    assert.equal(transform(code), `${IMPORT}\n${code}`)
+    assert.equal(transform(code), normalizeFormatting(`${IMPORT}\n${code}`))
   })
 
   it('only adds one import declaration', function () {
     const code = 'console.log(<div />);console.log(<h1 />);'
 
-    assert.equal(transform(code), `${IMPORT}\n${code}`)
+    assert.equal(transform(code), normalizeFormatting(`${IMPORT}\n${code}`))
   })
 
   it('does not add an import when h is already in scope', function () {
     const code = 'const h = args => args;const Foo = () => <div />;'
 
-    assert.equal(transform(code), code)
+    assert.equal(transform(code), normalizeFormatting(code))
   })
 
   it('does not add an import when jsx is not used', function () {
     const code = 'console.log(10);'
 
-    assert.equal(transform(code), code)
+    assert.equal(transform(code), normalizeFormatting(code))
   })
 })
